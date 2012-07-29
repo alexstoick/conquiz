273d8841588945544d2737b9fb3e9c8c21127c15
StoicaVlad <vlad.stoick@yahoo.co.uk>
2012-07-29 17:38:29 +0300

    addedAButtonToDemo

--- JS VERSION/js.js
+++ JS VERSION/js.js
@@ -1,14 +1,28 @@
 window.onload=main;
 function main(){
-	showPopUp('Question',["Answer1","Answer2","Answer3","Answer4"],this);
+	
 	drawCanvas();
-	function showPopUp(intrebare,answers){
-		$('#question').text(intrebare);
-		$('#answer1').text(answers[0]);
-		$('#answer2').text(answers[1]);
-		$('#answer3').text(answers[2]);
-		$('#answer4').text(answers[3]);
+	tryButton();
+	function tryButton ()
+	{
+		$("#addQuestion").click(spawn);
+		function spawn(){
+			showPopUp('Question',["Answer1","Answer2","Answer3","Answer4"],this);
+		};
 	}
+	function showPopUp(intrebare,answers){
+		$('#pop-up').show();
+		$('#question').text(intrebare);
+		$('#answer1').text(answers[0]).click(clicked);
+		$('#answer2').text(answers[1]).click(clicked);
+		$('#answer3').text(answers[2]).click(clicked);
+		$('#answer4').text(answers[3]).click(clicked);
+		function clicked()
+		{
+			console.log(this);
+			$('#pop-up').hide();
+		};
+	};
 	function drawCanvas()
 	{
 		var drawingCanvas = document.getElementById('canvas');
@@ -16,6 +30,6 @@ function main(){
 		context.rect(0, 0, 960, 500);
   		context.fillStyle = "#FFFF00";
   		context.fill();
-	}
+	};
 
 }
