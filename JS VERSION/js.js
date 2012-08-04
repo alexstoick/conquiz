9d8005d8ff7ad3c692fa2cea66b19d1de7e20ef7
Stoica Vlad <vlad.stoick@yahoo.co.uk>
2012-08-04 21:58:22 +0300

    added js v0.1
    Added timer + timeout and submitAnswer function

--- JS VERSION/js.js
+++ JS VERSION/js.js
@@ -1,29 +1,60 @@
-window.onload=main;
-function main(){
+
+function main() {
 	
 	drawCanvas();
 	tryButton();
+   
 	function tryButton ()
 	{
 		$("#addQuestion").click(spawn);
 		function spawn(){
 			showPopUp('Compozitori: În ce oraş a decedat Camille Saint-Saëns, compozitor francez din epoca romantică?'
-                      ,[" Alger1"," Alger2"," Alger3"," Alger4"],this);
+                      ,["Alger1","Alger2","Alger3","Alger4"],this);
 		};
 	}
-	function showPopUp(intrebare,answers){
+    //timer related
+    var timer=0,interval,timeout;
+    function addTime()
+    {
+        timer+=10;
+    }
+    function passedTime()
+    {
+        clearInterval(interval);
+        submitAnswer(0,10000);
+    }
+    function setUpTimer()
+    {
+        timer=0;
+        interval=setInterval(addTime,10);
+        timeout=setTimeout(passedTime,10000);
+    }
+    //show Questions related
+	function showPopUp (intrebare,answers) {
+        setUpTimer();
 		$('#pop-up').show();
 		$('#question').text(intrebare);
-		$('#answer1').text(answers[0]).click(clicked);
-		$('#answer2').text(answers[1]).click(clicked);
-		$('#answer3').text(answers[2]).click(clicked);
-		$('#answer4').text(answers[3]).click(clicked);
-		function clicked()
-		{
-			console.log(this);
-			$('#pop-up').hide();
-		};
+		$('#answer1').text(answers[0]).click(clickedAnswer);
+		$('#answer2').text(answers[1]).click(clickedAnswer);
+		$('#answer3').text(answers[2]).click(clickedAnswer);
+		$('#answer4').text(answers[3]).click(clickedAnswer);
+		
 	};
+    //Answer related
+    function clickedAnswer()
+		{
+			submitAnswer($(this).text(),timer);
+            clearInterval(interval);
+            clearTiemeout(timeout);
+		};
+    function submitAnswer(chosen , timeInMS )
+    {
+        console.log(chosen,timeInMS);
+        $('#pop-up').hide();
+        clearInterval(interval);
+        clearTimeout(timeout);
+    }
+    //map related
 	function drawCanvas()
 	{
 		var drawingCanvas = document.getElementById('canvas');
@@ -32,5 +63,10 @@ function main(){
   		context.fillStyle = "#221E1D";
   		context.fill();
 	};
+   
+    
 
 }
+$(document).ready(function() {
+   main();
+ });
